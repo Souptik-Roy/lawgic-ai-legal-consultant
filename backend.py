8a362cb5fd786aa712ec5631e9655c9dbffd8b8e
Souptik Roy <cs2231.diatm@gmail.com>
2026-04-13 08:33:23 +0530

    backend is doen

--- backend.py
+++ backend.py
@@ -6,14 +6,8 @@ app=FastAPI()
 
 class Query(BaseModel):
     query:str
-    
-    
-# @app.get("/")
-# def read_root():    
-#     return {"Server is running. Send a POST request to /query with your legal question."}
-#actual endpoint to handle the query and return the response
+ 
 @app.post("/query")
 def handle_query(query:Query):
     answer=rag_pipeline(query.query)
-    # answer = "This is a placeholder response. The RAG pipeline is currently under development."
     return {"Responses from LawGic\n": answer}
